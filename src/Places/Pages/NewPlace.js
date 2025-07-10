4865ee97af91c974e8b10edd57d51907160edd16
Equis Dev <dvaghasiya766@outlook.com>
2025-07-10 15:30:21 +0530

    Input

--- src/Places/Pages/NewPlace.js
+++ src/Places/Pages/NewPlace.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React from "react";
+
+import Input from "../../shared/components/FormElements/Input";
+// import Button from "../../shared/components/FormElements/Button";
+import "./NewPlace.css";
 
 const NewPlace = () => {
-  return <form className='place-form'>
-    
-  </form>;
+  return (
+    <form className="place-form">
+      <Input
+        // id="title"
+        element="input"
+        type="text"
+        label="Title"
+        // placeholder="Title"
+        // validators={[]}
+        // onInput={() => {}}
+        errorText="Please enter a valid title."
+      />
+      {/* <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        rows={5}
+        placeholder="Description"
+        validators={[]}
+        errorText="Please enter a valid description (at least 5 characters)."
+      />
+      <Input
+        id="address"
+        element="input"
+        type="text"
+        label="Address"
+        placeholder="Address"
+        validators={[]}
+        errorText="Please enter a valid address."
+      /> 
+      <button type="submit">ADD PLACE</button> */}
+    </form>
+  );
 };
 
 export default NewPlace;
