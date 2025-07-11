876d2bddbb0bef5780eaac67a25036c745cc040d
Equis Dev <dvaghasiya766@outlook.com>
2025-07-11 10:13:14 +0530

    Add

--- src/Places/Pages/UserPlaces.js
+++ src/Places/Pages/UserPlaces.js
@@ -32,27 +32,28 @@ const DUMMY_PLACES = [
   },
   {
     id: "p3",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
+    title: "Effile Tower",
+    description:
+      "Gustave Eiffel's iconic, wrought-iron 1889 tower, with steps and elevators to observation decks.",
     imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+      "https://lh3.googleusercontent.com/gps-cs-s/AC9h4np77l--KHoPs-fxibzZnJ0IBGt3ldkX-33NBUr8rdI3c439PL8BODi2yZDY7dx8JUeU21iJkiaErDFtCWJ6grY8yI83nYK7MGXLixl1WBnVOv7dYcW6RfzRWCnX-q-8JuB-1cXZCmFW7WA=w408-h544-k-no",
     address: "20 W 34th St, New York, NY 10001",
     location: {
-      lat: 40.7484405,
-      lng: -73.9878584,
+      lat: 48.8585409,
+      lng: 2.2919231,
     },
     creator: "u1",
   },
   {
     id: "p4",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
+    title: "Statu Of Unity",
+    description: "",
     imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+      "https://lh3.googleusercontent.com/gps-cs-s/AC9h4nr0OgTx3RvJ2RI_krPRbZj5vdQvNhpHCq8vsBxcjX-tXA5xFdNwbPHz3sPL-RjFuHkeU4S6PheqNOhUk-A6h2fCUb9lWdPZIeP49c4bgWGOfZsjy0iP2wxFQdskq7kwHlf8VFu8nw=w408-h544-k-no",
     address: "20 W 34th St, New York, NY 10001",
     location: {
-      lat: 40.7484405,
-      lng: -73.9878584,
+      lat: 21.8565543,
+      lng: 73.7062583,
     },
     creator: "u1",
   },
--- src/Shared/Components/UIElements/Map.js
+++ src/Shared/Components/UIElements/Map.js
@@ -1,28 +1,42 @@
-import React, { useRef, useEffect } from 'react';
+// src/components/UIElements/Map.jsx
+import React, { useRef, useEffect } from "react";
+import "ol/ol.css";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+import { fromLonLat } from "ol/proj";
 
-import './Map.css';
-
-const Map = props => {
+const MapComponent = ({ center, zoom }) => {
   const mapRef = useRef();
-  
-  const { center, zoom } = props;
 
   useEffect(() => {
-    const map = new window.google.maps.Map(mapRef.current, {
-      center: center,
-      zoom: zoom
+    const map = new Map({
+      target: mapRef.current,
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: fromLonLat([center.lng, center.lat]), // Convert to map projection
+        zoom: zoom,
+      }),
     });
-  
-    new window.google.maps.Marker({ position: center, map: map });
-  }, [center, zoom]);  
+
+    return () => map.setTarget(null);
+  }, [center, zoom]);
 
   return (
     <div
       ref={mapRef}
-      className={`map ${props.className}`}
-      style={props.style}
+      style={{
+        width: "100%",
+        height: "200px",
+        borderRadius: "8px",
+        overflow: "hidden",
+      }}
     ></div>
   );
 };
 
-export default Map;
+export default MapComponent;
