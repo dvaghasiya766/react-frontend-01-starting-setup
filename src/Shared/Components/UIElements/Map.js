df38f2c403b12de5b90ad42da08039f9600507dc
Equis Dev <dvaghasiya766@outlook.com>
2025-07-23 13:52:16 +0530

    Part 1

--- src/Shared/Components/UIElements/Map.js
+++ src/Shared/Components/UIElements/Map.js
@@ -1,20 +1,58 @@
-import React, { useRef, useEffect } from 'react';
+// import React, { useRef, useEffect } from "react";
 
-import './Map.css';
+// import "./Map.css";
 
-const Map = props => {
+// const Map = (props) => {
+//   const mapRef = useRef();
+
+//   const { center, zoom } = props;
+
+//   useEffect(() => {
+//     const map = new window.google.maps.Map(mapRef.current, {
+//       center: center,
+//       zoom: zoom,
+//     });
+
+//     new window.google.maps.Marker({ position: center, map: map });
+//   }, [center, zoom]);
+
+//   return (
+//     <div
+//       ref={mapRef}
+//       className={`map ${props.className}`}
+//       style={props.style}
+//     ></div>
+//   );
+// };
+
+// export default Map;
+import React, { useRef, useEffect } from "react";
+import "./Map.css";
+
+const Map = (props) => {
   const mapRef = useRef();
-  
   const { center, zoom } = props;
 
   useEffect(() => {
     const map = new window.google.maps.Map(mapRef.current, {
+      zoom: zoom,
       center: center,
-      zoom: zoom
+      mapId: "40443c5a815f5d694984a2f1", // Required for Advanced Markers
     });
-  
-    new window.google.maps.Marker({ position: center, map: map });
-  }, [center, zoom]);  
+
+    // ✅ Check if AdvancedMarkerElement is available
+    if (window.google?.maps?.marker?.AdvancedMarkerElement) {
+      new window.google.maps.marker.AdvancedMarkerElement({
+        map,
+        position: center,
+        title: "Uluru",
+      });
+    } else {
+      console.error(
+        "AdvancedMarkerElement is not available. Did you load the 'marker' library?"
+      );
+    }
+  }, [center, zoom]);
 
   return (
     <div
--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -3,6 +3,9 @@ import React, { useState, useContext } from "react";
 import Card from "../../shared/components/UIElements/Card";
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
@@ -10,11 +13,14 @@ import {
 } from "../../shared/util/validators";
 import { useForm } from "../../shared/hooks/form-hook";
 import { AuthContext } from "../../shared/context/auth-context";
+
 import "./Auth.css";
 
 const Auth = () => {
   const auth = useContext(AuthContext);
   const [isLoginMode, setIsLoginMode] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   const [formState, inputHandler, setFormData] = useForm(
     {
@@ -56,74 +62,163 @@ const Auth = () => {
 
   const authSubmitHandler = async (event) => {
     event.preventDefault();
+    setIsLoading(true);
 
     if (isLoginMode) {
-    } else {
       try {
-        const response = await fetch("localhost:5000/api/users/signup", {
+        // localhost:5000/api/users/login
+        const response = await fetch("http://192.168.1.67:5000/api/users/login", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
-            name: formState.inputs.name.value,
             email: formState.inputs.email.value,
             password: formState.inputs.password.value,
           }),
         });
 
         const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
         console.log(responseData);
+        setIsLoading(false);
+        auth.login();
       } catch (err) {
+        setIsLoading(false);
         console.log(err);
+        setError(err.message || "Something Went Wrong...");
+      }
+    } else {
+      try {
+        const response = await fetch(
+          "http://192.168.1.67:5000/api/users/signup",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              name: formState.inputs.name.value,
+              email: formState.inputs.email.value,
+              password: formState.inputs.password.value,
+            }),
+          }
+        );
+
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        console.log(responseData);
+        setIsLoading(false);
+        auth.login();
+      } catch (err) {
+        setIsLoading(false);
+        console.log(err);
+        setError(err.message || "Something Went Wrong...");
       }
     }
+  };
 
-    auth.login();
+  const errorHandler = () => {
+    setError(null);
   };
 
   return (
-    <Card className="authentication">
-      <h2>Login Required</h2>
-      <hr />
-      <form onSubmit={authSubmitHandler}>
-        {!isLoginMode && (
+    // <React.Fragment>
+    //   <ErrorModal error={error} onClear={errorHandler} />
+    //   <Card className="authentication">
+    //     {isLoading && <LoadingSpinner asOverLay />}
+    //     <h2>Login Required</h2>
+    //     <hr />
+    //     <form onSubmit={authSubmitHandler}>
+    //       {!isLoginMode && (
+    //         <Input
+    //           element="input"
+    //           id="name"
+    //           type="text"
+    //           label="Your Name"
+    //           validators={[VALIDATOR_REQUIRE()]}
+    //           errorText="Please enter a name."
+    //           onInput={inputHandler}
+    //         />
+    //       )}
+    //       <Input
+    //         element="input"
+    //         id="email"
+    //         type="email"
+    //         label="E-Mail"
+    //         validators={[VALIDATOR_EMAIL()]}
+    //         errorText="Please enter a valid email address."
+    //         onInput={inputHandler}
+    //       />
+    //       <Input
+    //         element="input"
+    //         id="password"
+    //         type="password"
+    //         label="Password"
+    //         validators={[VALIDATOR_MINLENGTH(5)]}
+    //         errorText="Please enter a valid password, at least 5 characters."
+    //         onInput={inputHandler}
+    //       />
+    //       <Button type="submit" disabled={!formState.isValid}>
+    //         {isLoginMode ? "LOGIN" : "SIGNUP"}
+    //       </Button>
+    //     </form>
+    //     <Button inverse onClick={switchModeHandler}>
+    //       SWITCH TO {isLoginMode ? "SIGNUP" : "LOGIN"}
+    //     </Button>
+    //   </Card>
+    // </React.Fragment>
+        <React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler} />
+      <Card className="authentication">
+        {isLoading && <LoadingSpinner asOverlay />}
+        <h2>Login Required</h2>
+        <hr />
+        <form onSubmit={authSubmitHandler}>
+          {!isLoginMode && (
+            <Input
+              element="input"
+              id="name"
+              type="text"
+              label="Your Name"
+              validators={[VALIDATOR_REQUIRE()]}
+              errorText="Please enter a name."
+              onInput={inputHandler}
+            />
+          )}
           <Input
             element="input"
-            id="name"
-            type="text"
-            label="Your Name"
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText="Please enter a name."
+            id="email"
+            type="email"
+            label="E-Mail"
+            validators={[VALIDATOR_EMAIL()]}
+            errorText="Please enter a valid email address."
             onInput={inputHandler}
           />
-        )}
-        <Input
-          element="input"
-          id="email"
-          type="email"
-          label="E-Mail"
-          validators={[VALIDATOR_EMAIL()]}
-          errorText="Please enter a valid email address."
-          onInput={inputHandler}
-        />
-        <Input
-          element="input"
-          id="password"
-          type="password"
-          label="Password"
-          validators={[VALIDATOR_MINLENGTH(5)]}
-          errorText="Please enter a valid password, at least 5 characters."
-          onInput={inputHandler}
-        />
-        <Button type="submit" disabled={!formState.isValid}>
-          {isLoginMode ? "LOGIN" : "SIGNUP"}
+          <Input
+            element="input"
+            id="password"
+            type="password"
+            label="Password"
+            validators={[VALIDATOR_MINLENGTH(5)]}
+            errorText="Please enter a valid password, at least 5 characters."
+            onInput={inputHandler}
+          />
+          <Button type="submit" disabled={!formState.isValid}>
+            {isLoginMode ? 'LOGIN' : 'SIGNUP'}
+          </Button>
+        </form>
+        <Button inverse onClick={switchModeHandler}>
+          SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
         </Button>
-      </form>
-      <Button inverse onClick={switchModeHandler}>
-        SWITCH TO {isLoginMode ? "SIGNUP" : "LOGIN"}
-      </Button>
-    </Card>
+      </Card>
+    </React.Fragment>
   );
 };
 
